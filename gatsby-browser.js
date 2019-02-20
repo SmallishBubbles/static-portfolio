d5a9d8b00dd7308e292264fd4b38cc9c4341c60d
MarishaHoza <marisha.hoza@gmail.com>
2019-02-20 01:28:54 -0800

    fix scroll position bug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,17 @@
 // this used to point to a global css sheet
 // however, styling has been updated to use styled components
+
+
+exports.onRouteUpdate = args => {
+  if (typeof window !== 'undefined' && args.action == "PUSH") {
+    window.scrollTo(0, 0)
+  }
+}
+
+exports.shouldUpdateScroll = args => {
+  //  scroll on < back, forward >, refresh, but not link clicks
+  return (
+    args.prevRouterProps == null ||
+    (args.prevRouterProps && args.prevRouterProps.history.action == "POP")
+  ) ? true : false;
+};
